225dab346b77bad0681672b156738733c73c7674
Paul Beynon <paul@beynon.app>
2022-12-01 10:10:12 +1100

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2022
+
+A repo to collection my solutions this year.
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+/* eslint-disable no-console */
+import { config } from '@src/init';
+
+console.log('Hello Code Advent 2022!');
+console.log(config);
