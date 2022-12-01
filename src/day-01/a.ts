c6d6c762b30a6168128a489de5bcf9ee553e53ff
Paul Beynon <paul@beynon.app>
2022-12-01 19:29:53 +1100

    DAY-01: Solve Part A

--- src/day-01/a.ts
+++ src/day-01/a.ts
@@ -4,4 +4,55 @@ import * as path from 'path';
 const inputFilepath = path.resolve(__dirname, './input.txt');
 const data = fs.readFileSync(inputFilepath, 'utf-8');
 
-console.log(data);
+
+/**
+ * The Elf data boundary is two new lines.
+ *
+ * @param input
+ */
+function extractElfBlocks(input: string): string[] {
+  return input.split('\n\n');
+}
+
+/**
+ * Add numeric value to the sum total.
+ *
+ * @param sum
+ * @param row
+ */
+function sumItems(sum: number, row: string): number {
+  return sum + Number(row);
+}
+
+/**
+ * An Elf's total calories is defined by the sum of each
+ * row in their data block.
+ *
+ * @param elf
+ */
+function reduceElfCalories(elf: string): number {
+  const calories = elf.split('\n');
+
+  return calories.reduce(sumItems, 0);
+}
+
+/**
+ * Serialise input data into calories.
+ */
+export const elfCalories = extractElfBlocks(data)
+  .map((elf) => reduceElfCalories(elf));
+
+let maxCalory = 0;
+export const log = [];
+
+elfCalories.forEach((calory, index) => {
+  if (calory > maxCalory) {
+    maxCalory = calory;
+    log.push({
+      i: elfCalories.indexOf(maxCalory),
+      v: maxCalory,
+    });
+  }
+});
+
+console.log(maxCalory);
