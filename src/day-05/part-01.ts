82107a1a92acbe4146e188986c6f3d98553a73e9
Paul Beynon <paul@beynon.app>
2022-12-12 14:05:23 +1100

    DAY-05: Do some refactoring before part 2

--- src/day-05/part-01.ts
+++ src/day-05/part-01.ts
@@ -1,22 +1,20 @@
-import { getInput, inspect } from '@src/lib';
+import { array, getInput, inspect } from '@src/lib';
 
+
+type Stack = string[][];
 
 /**
  * Representation of the stack as rows and columns with labels.
  */
 type StackData = {
-  rows: string[][];
-  cols: string[][];
+  rows: Stack;
+  cols: Stack;
   labels: number[];
 };
 
 type MoveInstruction = Record<'move' | 'from' | 'to', number>;
 
 const EMPTY_ROW = '---';
-
-function rotate<T>(array: T[][]): T[][] {
-  return array[0].map((_, i) => array.map(row => row[i]));
-}
 
 /**
  * Split a row into its column stacks, populating empty stacks with EMPTY_ROW.
@@ -34,35 +32,6 @@ function splitRowToStack(row: string): string[] {
 
 
 /**
- * Find the maximum array length in the provided arrays.
- *
- * @param arrays
- */
-function getMaxLength(...arrays: unknown[][]): number {
-  return arrays.reduce((max, array) => Math.max(max, array.length), 0);
-}
-
-/**
- * Fill an array with EMPTY_VALUE up to length.
- *
- * @param array
- * @param max
- */
-function fillArray(array: string[], max: number): string[] {
-  const length = max - array.length;
-
-  if (!length) {
-    return array;
-  }
-
-  return [
-    ...array,
-    ...Array.from({ length }).fill(EMPTY_ROW),
-  ] as string[];
-}
-
-
-/**
  * Parse the stack from input data and return its rows and columns, with labels.
  *
  * @param input
@@ -74,21 +43,21 @@ function prepareStackRows(input: string, splitIndex: number): StackData {
     .filter(Boolean)
     .map(splitRowToStack);
 
-  const max = getMaxLength(mapped);
+  const max = array.getMaxLength(mapped);
 
   const rows = [
-    fillArray([], max),
-    ...mapped.map((row) => fillArray(row, max)),
+    ...mapped.map((row) => array.fillArray(row, max, EMPTY_ROW)),
   ];
 
   const labels = rows.pop().map((label) => Number(label));
 
-  const cols = rotate(rows).map((col) => col.filter(val => val !== EMPTY_ROW));
+  const cols = array.rotate(rows).map((col) => col.filter(val => val !== EMPTY_ROW));
 
   return { labels, cols, rows };
 }
 
 /**
+ * Parse the list of moves from input data in Move Instructions.
  *
  * @param input
  * @param splitIndex
@@ -109,6 +78,24 @@ function prepareMovesList(input: string, splitIndex: number): MoveInstruction[] 
     });
 }
 
+/**
+ * Move items from one stack to another one at a time.
+ *
+ * @param labels
+ * @param cols
+ * @param move
+ * @param from
+ * @param to
+ */
+function moveSingleItems(labels: number[], cols: Stack, { move, from, to }: MoveInstruction): void {
+  const fromCol = cols[labels.indexOf(from)];
+  const toCol = cols[labels.indexOf(to)];
+
+  while (move--) {
+    toCol.unshift(fromCol.shift());
+  }
+}
+
 
 if (require.main === module) {
   console.log('Day 5 - Part 1');
@@ -116,46 +103,18 @@ if (require.main === module) {
   const data = getInput(__dirname);
   const moveIndex = data.indexOf('move');
 
-  const { rows, cols, labels } = prepareStackRows(data, moveIndex);
-
-  const doMove = ({ move, from, to }: MoveInstruction): void => {
-    const fromCol = cols[labels.indexOf(from)];
-    const toCol = cols[labels.indexOf(to)];
-
-    while (move--) {
-      toCol.unshift(fromCol.shift());
-    }
-  };
-
+  const { cols, labels } = prepareStackRows(data, moveIndex);
   const moves = prepareMovesList(data, moveIndex);
 
-  inspect('rows');
-  inspect([
-    [],
-    ...rows,
-    [],
-  ]);
-
-
-  inspect('cols before');
-  inspect([
-    [],
-    ...cols,
-    [],
-  ]);
-
   for (const move of moves) {
-    doMove(move);
+    moveSingleItems(labels, cols, move);
   }
 
-  inspect('cols after');
-  inspect([
-    [],
-    ...cols,
-    [],
-  ]);
-
   const result = cols.reduce((str, col) => str + col.shift(), '');
-  inspect(cols[labels.indexOf(1)]);
+
+  if (result !== 'GFTNRBZPF') {
+    throw new RangeError('Result does not match expected value');
+  }
+
   inspect(result);
 }
--- src/lib/array.ts
+++ src/lib/array.ts
@@ -35,3 +35,46 @@ export function batches<T>(input: T[], size = 3): T[][] {
 
   return batches;
 }
+
+/**
+ * Find the maximum array length in the provided arrays.
+ *
+ * @param arrays
+ */
+export function getMaxLength(...arrays: unknown[][]): number {
+  return arrays.reduce((max, array) => Math.max(max, array.length), 0);
+}
+
+/**
+ * Expand the given `array` to the `max` length filled with the given `value`.
+ *
+ * @param array
+ * @param max
+ * @param value
+ */
+export function fillArray(array: string[], max: number, value = '-'): string[] {
+  const length = max - array.length;
+
+  if (!length) {
+    return array;
+  }
+
+  return [
+    ...array,
+    ...Array.from({ length }).fill(value),
+  ] as string[];
+}
+
+/**
+ * Given a multidimensional array, rotate the contents. This is a naive implementation,
+ * and will not work for arrays of varying lengths.
+ *
+ * @param array
+ * @example ```typescript
+ *  rotate([0, 1], [2, 3]) // => [0, 2], [1, 3]
+ * ```
+ *
+ */
+export function rotate<T>(array: T[][]): T[][] {
+  return array[0].map((_, i) => array.map(row => row[i]));
+}
