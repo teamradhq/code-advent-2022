79ad091db3820c3294435404b4cbf12275a4b6a0
Paul Beynon <paul@beynon.app>
2022-12-12 14:51:36 +1100

    DAY-06: Solve part 1

--- src/day-06/part-01.ts
+++ src/day-06/part-01.ts
@@ -1,9 +1,47 @@
-import { getInput } from '@src/lib';
+import { getInput, inspect } from '@src/lib';
+
+const markerLength = 4;
+
+/**
+ * Given the input, return the first set of non-repeating characters with a length of 4.
+ *
+ * @param input
+ */
+function findNonRepeatingCharacters(input: string): number {
+  const characters = [...input];
+
+  for (const [index] of characters.entries()) {
+    const sequence = characters.slice(index, index + markerLength);
+    const unique = new Set(sequence);
+
+    if (unique.size === markerLength) {
+      return index + markerLength;
+    }
+  }
+
+  return -1;
+}
 
 if (require.main === module) {
-  console.log('Day 5 - Part 1');
+  console.log('Day 6 - Part 1');
 
   const data = getInput(__dirname);
 
-  console.log(data);
+  const examples: [string, number][] = [
+    ['mjqjpqmgbljsphdztnvjfqwrcgsmlb', 7],
+    ['bvwbjplbgvbhsrlpgdmjqwftvncz', 5],
+    ['nppdvjthqldpwncqszvftbrmjlhg', 6],
+    ['nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 10],
+    ['zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 11],
+  ];
+
+  for (const [input, expected] of examples) {
+    const actual = findNonRepeatingCharacters(input);
+
+    if (actual !== expected) {
+      throw new ReferenceError(`Expected ${expected} but got ${actual}`);
+    }
+  }
+
+  inspect((findNonRepeatingCharacters(data)));
 }
